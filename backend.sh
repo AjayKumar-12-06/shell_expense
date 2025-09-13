e962c92a091f0bcee9b5642778c34f75073ab5e1
AjayKumar-12-06 <meajaykumar.p97@gmail.com>
2025-09-13 21:09:25 +0530

    created backend server

--- backend.sh
+++ backend.sh
@@ -11,7 +11,7 @@ Y="\e[33m"
 N="\e[0m"
 
 #creating logs folder
-LOG_FOLDER="/var/log/mysql.logs"
+LOG_FOLDER="/var/log/backend.logs"
 mkdir -p $LOG_FOLDER
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
@@ -34,18 +34,18 @@ validate() {
     fi
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
 validate $? "disable nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
 validate $? "enable nodejs:20"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOG_FILE_NAME
 validate $? "installing nodejs"
 
-id expense
+id expense &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]; then
-     useradd expense 
+     useradd expense &>>$LOG_FILE_NAME
      validate $? "adding expense"
 else
     echo -e "user already exists : $Y SKKIPP $N"
@@ -55,7 +55,7 @@ mkdir -p /app
 
 rm -rf /app/*
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 validate $? "downloading the code"
 
 cd /app
@@ -64,23 +64,23 @@ validate $? "change the app directory"
 unzip /tmp/backend.zip
 validate $? "unzip the code"
 
-npm install
+npm install &>>$LOG_FILE_NAME
 validate $? "dependencies installing"
 
-cp /home/ec2-user/SHELL_EXPENSE/backend.service /etc/systemd/system/backend.service
+cp /home/ec2-user/SHELL_EXPENSE/backend.service /etc/systemd/system/backend.service &>>$LOG_FILE_NAME
 validate $? "moving the data to server"
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOG_FILE_NAME
 validate $? "installing mysql"
 
-mysql -h 172.31.28.73 -u root -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.28.73 -u root -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
 validate $? "creating schema"
 
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOG_FILE_NAME
 validate $? "daemon reload"
 
-systemctl restart backend
+systemctl restart backend &>>$LOG_FILE_NAME
 validate $? "restarted backend"
 
-systemctl enable backand
+systemctl enable backand &>>$LOG_FILE_NAME
 validate $? "enable backend"
